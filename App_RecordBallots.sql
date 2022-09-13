3fd0f6a1342ebe61bacc31853f1fa68e013845a8
Heber Allen <Heber.Allen@snow.edu>
2022-09-13 15:53:13 -0600

    Instructions for running/testing: App_RecordBallots.sql

--- App_RecordBallots.sql
+++ App_RecordBallots.sql
@@ -1,3 +1,13 @@
-call sim_voter_bulk(5,5,'bulk5');
+--After building the database, these steps would generate 20,000 ballots for the default election.
+call sim_ballots_bulk(1,10000,1000,'precinct A');
+call sim_ballots_bulk(1,10000,1000,'precinct B');
+update election set ballotingclosed=true where id=1;
+call TabulateBallotsForOffice(1,1);
+call TabulateBallotsForOffice(2,1);
+call TabulateBallotsForOffice(3,1);
+call TabulateBallotsForOffice(4,1);
+call TabulateBallotsForOffice(5,1);
+call TabulateBallotsForOffice(6,1);
+select * from vResultsPivot where election_id=1;
 
-Call sim_voter_bulk(50000,5000,'bulk50k'); 
+
